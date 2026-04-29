4ffd7fbcffe57b66d74377e435afeaf7584465fa
Chaimae Hakam <chaimaahakam@gmail.com>
2026-04-29 21:10:57 +0100

    initialize Spring Boot backend and configure MySQL

---
+++ backend/src/main/java/com/somap/backend/SomapBackendApplication.java
@@ -0,0 +1,13 @@
+package com.somap.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomapBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SomapBackendApplication.class, args);
+	}
+
+}
