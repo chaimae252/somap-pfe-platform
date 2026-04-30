18963007139d6b3d5be47c7842b2a8936682389c
Chaimae Hakam <chaimaahakam@gmail.com>
2026-04-30 02:18:38 +0100

    implement utilisateur management module

--- backend/src/main/java/com/somap/backend/service/UtilisateurService.java
+++ backend/src/main/java/com/somap/backend/service/UtilisateurService.java
@@ -1,4 +1,14 @@
 package com.somap.backend.service;
 
+import com.somap.backend.dto.UtilisateurDTO;
+
+import java.util.List;
+
 public interface UtilisateurService {
+
+    List<UtilisateurDTO> getAllUsers();
+
+    UtilisateurDTO getUserById(Long id);
+
+    void deleteUser(Long id);
 }
--- backend/src/main/java/com/somap/backend/service/impl/UtilisateurServiceImpl.java
+++ backend/src/main/java/com/somap/backend/service/impl/UtilisateurServiceImpl.java
@@ -1,4 +1,26 @@
 package com.somap.backend.service.impl;
 
-public class UtilisateurServiceImpl {
+import com.somap.backend.dto.UtilisateurDTO;
+import com.somap.backend.service.UtilisateurService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UtilisateurServiceImpl implements UtilisateurService {
+
+    @Override
+    public List<UtilisateurDTO> getAllUsers() {
+        return null;
+    }
+
+    @Override
+    public UtilisateurDTO getUserById(Long id) {
+        return null;
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+
+    }
 }
